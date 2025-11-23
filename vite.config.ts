45d55c2d703bdee40d4445a486868ac3445c25ec
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-11-23 14:44:57 +0100

    chore: setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/conversion-chart/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 });
